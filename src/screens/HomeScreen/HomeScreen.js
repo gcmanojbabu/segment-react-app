d48f9d79aa72ef251024b0a104560f353f1c3a05
gcmanojbabu <gcmanojbabu@gmail.com>
2022-12-02 01:12:54 +0530

    feat: Update style

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,12 +1,14 @@
-import { Autocomplete, Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid, Link, TextField } from '@mui/material'
+import { AppBar, Autocomplete, Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid, IconButton, Link, TextField, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import CircleIcon from '@mui/icons-material/Circle';
 import axios from 'axios';
+import { Box, Container } from '@mui/system';
 
 export default function HomeScreen() {
   const [openPopup, setOpenPopup] = useState(false);
   const [segmentName, setSegmentName] = useState('')
   const [segmentValue, setSegmentValue] = useState('')
+  const [segmentNameError, setSegmentNameError] = useState(false)
   const [segmentValueError, setSegmentValueError] = useState(false)
   const [schemaList, setSchemaList] = useState([])
   const initialSchemaOptions = [
@@ -72,6 +74,17 @@ export default function HomeScreen() {
   }
 
   const onSaveSegment = async () => {
+    // validation
+    setSegmentValueError(false)
+    if (schemaList.length === 0 || segmentName === '') {
+      setSegmentValueError(true)
+      setSegmentNameError(true)
+      return false
+    }
+
+
+
+
     let newSchemaList = []
     for (const element of schemaList) {
       let valueToSchemaList = {}
@@ -91,11 +104,19 @@ export default function HomeScreen() {
 
   return (
     <>
-      <Grid>
-        <h1>
-          View Audience
-        </h1>
+      <Box>
+        <AppBar position="static" style={{
+          backgroundColor: '#2596be',
+          boxShadow: 'none',
+          padding: '15px'
+        }}>
+          <Typography variant="h4">
+            View Audience
+          </Typography>
+        </AppBar>
+
         <div>
+          <br />
           <Button variant="contained" onClick={onSaveSegmentClick}>Save segment</Button>
 
           <Dialog
@@ -104,69 +125,110 @@ export default function HomeScreen() {
             aria-labelledby="alert-dialog-title"
             aria-describedby="alert-dialog-description"
           >
-            <DialogTitle id="alert-dialog-title">
-              {"Saving segment"}
-            </DialogTitle>
-            <DialogContent>
-              <p>Enter the Name of the Segment</p>
-              <TextField id="outlined-basic" label="Name of the Segment" fullWidth variant="outlined" onChange={onSegmentNameChange} />
-              <p>To save your segment, you need to add the schemas to build the query</p>
-              <CircleIcon color="success" /> - User Traits
-              <CircleIcon sx={{ color: 'red' }} /> - Group Traits
+            <Box>
+              <Grid
+                container
+                direction="row"
+                justifyContent="flex-start"
+                alignItems="flex-start"
+              // style={{
+              //   marginTop: '30px',
+              // }}
+              >
+                <Grid item xs={12}>
+                  <Typography variant="h5" style={{
+                    color: 'white',
+                    backgroundColor: '#2596be',
+                    boxShadow: 'none',
+                    padding: '20px'
+                  }}>
+                    Saving segment
+                  </Typography>
+                </Grid>
 
-              {schemaList?.map((element, index) => (
-                <React.Fragment key={index}>
-                  <Autocomplete
-                    spacing={2}
-                    disablePortal
-                    id="combo-box"
-                    options={initialSchemaOptions}
-                    filterOptions={(options, params) => {
-                      let filtered = options.filter(record => !schemaOptionsFlag[record.value])
-                      return filtered;
-                    }}
-                    renderInput={(params) => <TextField {...params} />}
-                    onChange={(event, value) => onSegmentSchemaStateChange(event, value, index)}
-                    value={element}
-                    style={{
-                      marginTop: '30px',
-                      marginBottom: '30px'
-                    }}
-                  />
-                </React.Fragment>
-              ))}
-              <Grid item xs={12}>
-                <Autocomplete
-                  disablePortal
-                  id="combo-box"
-                  options={initialSchemaOptions}
-                  filterOptions={(options, params) => {
-                    let filtered = options.filter(record => !schemaOptionsFlag[record.value])
-                    return filtered;
-                  }}
-                  renderInput={(params) => <TextField {...params} label="Add schema to segment"
-                    error={segmentValueError}
-                    id="standard-error-helper-text"
-                    helperText={segmentValueError && "Please select value"}
-                  />}
-                  onChange={(event, value) => onSegmentSchemaChange(event, value)}
-                  value={segmentValue}
-                  style={{
-                    marginTop: '30px',
-                    marginBottom: '30px'
-                  }}
-                />
+                <DialogContent>
+                  <Grid item xs={12}>
+                    <Typography variant="subtitle1" gutterBottom>
+                      Enter the Name of the Segment
+                    </Typography>
+                    <TextField autoComplete='off' id="outlined-basic" label="Name of the Segment" fullWidth variant="outlined" onChange={onSegmentNameChange}
+                      error={segmentNameError}
+                      helperText={segmentNameError && "Please enter segment name"} />
+                    <Typography variant="subtitle1" gutterBottom sx={{ my: 2 }}>
+                      To save your segment, you need to add the schemas to build the query
+                    </Typography>
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Grid
+                      container
+                      direction="row"
+                      justifyContent="flex-end"
+                      alignItems="center"
+                    >
+                      <CircleIcon color="success" sx={{ height: '15px', width: '15px' }} />
+                      - User Traits
+                      <CircleIcon sx={{ color: '#AD027E', height: '15px', width: '15px', ml: 2 }} />
+                      - Group Traits
+
+                    </Grid>
+                  </Grid>
+                  {schemaList?.map((element, index) => (
+                    <React.Fragment key={index}>
+                      <Autocomplete
+                        spacing={2}
+                        disablePortal
+                        id="combo-box"
+                        options={initialSchemaOptions}
+                        filterOptions={(options, params) => {
+                          let filtered = options.filter(record => !schemaOptionsFlag[record.value])
+                          return filtered;
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                        onChange={(event, value) => onSegmentSchemaStateChange(event, value, index)}
+                        value={element}
+                        style={{
+                          marginTop: '30px',
+                          marginBottom: '30px'
+                        }}
+                      />
+                    </React.Fragment>
+                  ))}
+                  <Grid item xs={12}>
+                    {/* <CircleIcon color="success" sx={{ height: '15px', width: '15px' }} /> */}
+                    <Autocomplete
+                      disablePortal
+                      id="combo-box"
+                      options={initialSchemaOptions}
+                      filterOptions={(options, params) => {
+                        let filtered = options.filter(record => !schemaOptionsFlag[record.value])
+                        return filtered;
+                      }}
+                      renderInput={(params) => <TextField {...params} label="Add schema to segment"
+                        error={segmentValueError}
+                        id="standard-error-helper-text"
+                        helperText={segmentValueError && "Please select value"}
+                      />}
+                      onChange={(event, value) => onSegmentSchemaChange(event, value)}
+                      value={segmentValue}
+                      style={{
+                        marginTop: '30px',
+                        marginBottom: '30px'
+                      }}
+                    />
+                  </Grid>
+                  <Link onClick={addNewSchema}>+ Add new schema</Link>
+                </DialogContent>
+
+                <DialogActions>
+                  <Button variant="contained" onClick={onSaveSegment}>Save the Segment</Button>
+                  <Button onClick={handleClose}>Cancel</Button>
+                </DialogActions>
               </Grid>
-              <Link onClick={addNewSchema}>+ Add new schema</Link>
-            </DialogContent>
-
-            <DialogActions>
-              <Button variant="contained" onClick={onSaveSegment}>Save the Segment</Button>
-              <Button onClick={handleClose}>Cancel</Button>
-            </DialogActions>
+            </Box>
           </Dialog>
         </div>
-      </Grid>
+      </Box>
     </>
   )
 }
